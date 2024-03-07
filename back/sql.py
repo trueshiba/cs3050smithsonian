87d0b817a00ed0f8043edd1e87779b96f99bf783
kierajclarke <kjclarke@uvm.edu>
2024-03-06 19:23:31 -0500

    troubleshoot database connection

--- back/sql.py
+++ back/sql.py
@@ -1,20 +1,16 @@
 import sqlite3
+import csv
 
 conn = sqlite3.connect('Smithbase.db')
-
 
 # Create a cursor object to execute SQL commands
 cursor = conn.cursor()
 
-
-# Create a cursor object
-cursor = conn.cursor()
-
 # Execute an SQL CREATE TABLE statement to create a new table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS famous_people (
+    CREATE TABLE IF NOT EXISTS Smithbase (
         id INTEGER PRIMARY KEY,
-        full_name TEXT NOT NULL,
+        name TEXT NOT NULL,
         last_name TEXT NOT NULL,
         gender TEXT,
         nationality TEXT,
@@ -26,17 +22,19 @@ cursor.execute('''
     )
 ''')
 
+conn.commit()
+
 # Load data from CSV file
 with open('smithbase.csv', 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         # Insert each row from the CSV file into the database
         cursor.execute('''
-            INSERT INTO Smitbase (full_name, last_name, gender, nationality, occupation, birth_year, death_year, age, rating)
+            INSERT INTO Smithbase (name, last_name, gender, nationality, occupation, birth_year, death_year, age, rating)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-            row['Full Name'],
-            row['Last Name'],
+            row['Name'],
+            row['last_name'],
             row['Gender'],
             row['Nationality'],
             row['Occupation'],
@@ -45,8 +43,6 @@ with open('smithbase.csv', 'r', newline='', encoding='utf-8') as csvfile:
             int(row['Age']),
             int(row['Rating'])
         ))
-
-
 
 
 # Commit the transaction (save changes)
