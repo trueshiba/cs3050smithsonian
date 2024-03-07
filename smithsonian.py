376d37c54e2492d90b463b28984ec7d27f41ce8a
trueshiba <107582031+trueshiba@users.noreply.github.com>
2024-03-06 19:03:57 -0500

    Added functions for database usage

--- smithsonian.py
+++ smithsonian.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template 
 import traceback
+import sqlite3
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    return conn
+
 
 app = Flask(__name__, template_folder='front', static_folder='front/css')
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    conn = get_db_connection()
+    rows = conn.cursor().fetchall()
+    conn.close()
+    return render_template('index.html', data=rows)
 
 
 if __name__ == '__main__':
