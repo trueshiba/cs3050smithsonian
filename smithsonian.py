37a906f1651740906d8c51f2cd3b2777375a2f73
kierajclarke <kjclarke@uvm.edu>
2024-03-26 16:49:16 -0400

    search function so far, however the flask app is not reflecting any changes to my work

--- smithsonian.py
+++ smithsonian.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 import traceback
 import sqlite3
 
@@ -26,10 +26,13 @@ def washingsmith():
     return render_template('smith_template.html', name=row[0][1], lastname=row[0][2], sex=row[0][3],
                            nat=row[0][4], occ=row[0][5], age=row[0][8], rate=row[0][9])
 
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    conn = get_db_connection()
+    rows = conn.execute("SELECT * FROM smithbase WHERE name LIKE ?", ('%' + query + '%',)).fetchall()
+    conn.close()
+    return render_template('index.html', rows=rows)
 
 if __name__ == '__main__':
-
-    try:
-        app.run(debug=app.debug, host='localhost', port=8097)
-    except Exception as err:
-        traceback.print_exc()
+     app.run(debug=True, host='localhost', port=8097)
