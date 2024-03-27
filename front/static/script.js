49f424a63b09f91d8af65431320e4fad68e2d03f
akaplan521 <alexa.kaplan@uvm.edu>
2024-03-27 18:49:32 -0400

    Implementing search function. Using AJAX to send requests to server.

--- front/static/script.js
+++ front/static/script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     var form = document.getElementById("form");
     var queryInput = document.getElementById("query");
-    var filterDropdown = document.getElementById("filter-dropdown-select"); 
+    var filterDropdown = document.getElementById("filter-dropdown-select");
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
 
         var query = queryInput.value;
         var filter = filterDropdown.value;
@@ -18,7 +17,19 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Search Query:", query);
         console.log("Filter Choice:", filter);
 
-        
- 
+        // Using AJAX to send data to flask
+        $.ajax({
+            type: "POST",
+            url: "/search", //route to flask search
+            contentType: "application/json", 
+            data: JSON.stringify(entries),
+            success: function(response) {
+                console.log(response); //log response in console
+                
+            },
+            error: function(xhr, status, error) {
+                console.error(error); 
+            }
+        });
     });
 });
--- smithsonian.py
+++ smithsonian.py
@@ -9,7 +9,7 @@ def get_db_connection():
 
 
 app = Flask(__name__, template_folder='front', static_folder='front/static')
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def hello():
@@ -18,25 +18,36 @@ def hello():
     conn.close()
     return render_template('index.html', rows=rows)
 
-@app.route('/test')
-def washingsmith():
-    conn = get_db_connection()
-    row = conn.execute("SELECT 1 FROM smithbase").fetchone()
-    conn.close()
-    return render_template('smith_template.html', name=row[1], lastname=row[2])
-
-@app.route('/search')
+@app.route('/search', methods=['POST'])
 def searchFunction():
+    try:
+        data = request.get_json()
+        search_query = data.get('searchQuery')
+        sort_method = data.get('sortMethod')
 
-    search_query = request.get_json().get('searchQuery')
-    sort_method = request.get_json().get('sortMethod')
+        print(search_query, sort_method)
 
-    conn = get_db_connection()
+        conn = get_db_connection()
+        
+        if len(sort_method) == 0:
+            query = "SELECT * FROM smithbase WHERE name LIKE ? OR last_name LIKE ? OR gender LIKE ? OR nationality LIKE ? OR occupation LIKE ? ORDER BY name ASC"
+            rows2 = conn.execute(query, ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%')).fetchall()
+        else:
+            query = "SELECT * FROM smithbase WHERE name LIKE ? OR last_name LIKE ? OR gender LIKE ? OR nationality LIKE ? OR occupation LIKE ? ORDER BY {} ASC".format(sort_method)
+            rows2 = conn.execute(query, ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%')).fetchall()
+        
+        conn.close()
 
-    query = ""
-    rows = conn.execute(query, (f'%{search_query}%', sort_method)).fetchall()
-    conn.close()
-    return render_template('smith_template.html', name=row[1], lastname=row[2])
+        print("Number of rows found:", len(rows2))
+        print("Rows:", rows2)
+
+        return render_template('index.html', rows=rows2)
+
+        #return jsonify(rows2)
+    
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
