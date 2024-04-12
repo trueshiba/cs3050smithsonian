a0f2da18430b55f6451e3e367d998805117d4a40
trueshiba <107582031+trueshiba@users.noreply.github.com>
2024-04-12 11:52:31 -0400

    Added helpful comments

--- smithsonian.py
+++ smithsonian.py
@@ -3,27 +3,38 @@ import traceback
 import sqlite3
 
 
+# Creates link to database that we can use in our other app routes
 def get_db_connection():
     conn = sqlite3.connect('back\smithbase.db')
     return conn
 
 
+# Defines flask app and specifys where to look for html and static resources
 app = Flask(__name__, template_folder='front', static_folder='front/static')
 
 
+# Base app route
 @app.route('/')
 def start():
+    # Auto redirects to .../home with code 302: 'redirect'
     return redirect(url_for("home"), code=302)
 
 
+# Home page for app. Gets all entries within database to display
 @app.route('/home')
 def home():
     conn = get_db_connection()
+    # Get all entries from database
     rows = conn.execute("SELECT * FROM smithbase").fetchall()
     conn.close()
+
+    # Debugging statement
+    if (app.debug == True): print(rows)
+
     return render_template('index.html', rows=rows)
 
 
+# Search route used after entering in search bar/ submitting on button to display the user query
 @app.route('/search', methods=['POST'])
 def searchFunction():
     try:
@@ -45,9 +56,12 @@ def searchFunction():
 
         conn.close()
 
+        
         #return render_template('index.html', rows=rows2)
         
-        print(rows2)
+        # Debugging statement
+        if (app.debug == True): print(rows2)
+
         return jsonify(rows2)
 
     except Exception as e:
@@ -55,20 +69,29 @@ def searchFunction():
         return jsonify({'error': str(e)})
 
 
-@app.route('/<string:id>', methods=['GET', 'POST'])
+# Individual 'Smith' profile page route
+# Uses the pmk_id of a given smith to query the datbase and fill in template page with correct values
+@app.route('/<int:id>', methods=['GET', 'POST'])
 def profile(id):
     conn = get_db_connection()
     
-    sqlQueryProfile = "SELECT * FROM smithbase WHERE id=" + id ## Easy sql injection attack here by changing what ID is!!!!
-    sqlQueryReviews = "SELECT rating, review FROM reviews INNER JOIN smithbase ON smithbase.id=reviews.smith_id WHERE smithbase.id=" + id
+    # Initial query to get smith profile info
+    sqlQueryProfile = "SELECT * FROM smithbase WHERE id=" + str(id) ## <----------------- Easy sql injection attack here by changing what ID is!!!!
+    # Secondary INNER JOIN query to get corresponding reviews of chosen smith
+    sqlQueryReviews = "SELECT rating, review FROM reviews INNER JOIN smithbase ON smithbase.id=reviews.smith_id WHERE smithbase.id=" + str(id)
+
     profileRow = conn.execute(sqlQueryProfile).fetchall()
     reviewRows = conn.execute(sqlQueryReviews).fetchall()
-    print(reviewRows)
+
+    # Debugging statement
+    if (app.debug == True): print(reviewRows)
+    
     conn.close()
     return render_template('smith_template.html', 
                             name=profileRow[0][1], lastname=profileRow[0][2], sex=profileRow[0][3], nat=profileRow[0][4], occ=profileRow[0][5], age=profileRow[0][8], rate=profileRow[0][9],
                             reviews=reviewRows
                             )
+
 
 
 #@app.route('/rate', methods=['POST'])
@@ -99,10 +122,10 @@ def profile(id):
 
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
 
     try:
         # Change debug=app.debug -> debug=True for auto reloading while coding and saving
-        app.run(debug=False, host='localhost', port=8097)
+        app.run(debug=app.debug, host='localhost', port=8097)
     except Exception as err:
         traceback.print_exc()
